0b2ae9fbfd54c1cdab348153c8fa0a992c0c0bf2
choieb9519 <choieb9519@gmail.com>
2021-06-21 16:32:33 +0900

    20210621

---
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,14 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.junit.Test;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+
+//	@Select("SELECT * FROM tbl_board")
+	public List<BoardVO> getList();
+	
+}
