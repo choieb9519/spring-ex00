f6145392d7b5c32912a6ee371ea828e82ad56c4f
choieb9519 <choieb9519@gmail.com>
2021-06-22 09:05:38 +0900

    20210622

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,8 +2,6 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
-import org.junit.Test;
 import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
@@ -11,4 +9,33 @@ public interface BoardMapper {
 //	@Select("SELECT * FROM tbl_board")
 	public List<BoardVO> getList();
 	
+	// INSERT INTO tbl_board (title, content, writer) 
+	// VALUES (#{title}, #{content}, #{writer})
+	public int insert(BoardVO board);
+	/*
+		String sql = "INSERT INTO tbl_board (title, content, writer) VALUES (?, ?, ?) ";
+		...
+		pstmt.setString(1, board.getTitle());
+		pstmt.setString(2, board.getContent());
+		pstmt.setString(3, board.getWriter());
+		
+		pstmt.executeUpdate();
+
+	 */
+	
+	public int insertSelectKey(BoardVO board);
+	
+	public BoardVO read(long bno);
+	
+	public int delete(long bno);
+	
+	public int update(BoardVO board);
 }
+
+
+
+
+
+
+
+
