4f953e95cd113aafecad034651de0ee599eb5af0
choieb9519 <choieb9519@gmail.com>
2021-06-29 16:15:44 +0900

    20210629

--- src/main/java/org/zerock/controller/lecture/rest/Ex10RestController.java
+++ src/main/java/org/zerock/controller/lecture/rest/Ex10RestController.java
@@ -3,6 +3,7 @@ package org.zerock.controller.lecture.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class Ex10RestController {
 	private List<User> list = new ArrayList<>();
+	private int like = 10;
 	
 	@PostMapping("/add")
 	public User addUser(User user) {
@@ -24,5 +26,24 @@ public class Ex10RestController {
 		log.info(list);
 		
 		return user;
+	}
+	
+	@GetMapping("/list")
+	public List<User> list() {
+		return list;
+	}
+	
+	@PostMapping("/like")
+	public Integer like() {
+		like++;
+		
+		return like;
+	}
+	
+	@PostMapping("/dislike")
+	public Integer dislike() {
+		like--;
+		
+		return like;
 	}
 }
